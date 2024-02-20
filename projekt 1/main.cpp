de5ef0f430d11ef5957fb59c87493b3abe022d85
Luke Bobinski <luke.bobinski@protonmail.com>
2024-02-20 18:54:42 +0100

    Initial Commit

---
+++ projekt 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  projekt 1
+//
+//  Created by Łukasz Bobiński on 20/02/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
